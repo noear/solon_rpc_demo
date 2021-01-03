72ee83409946b52401327cb3163b94f36e863d94
noear <noear@live.cn>
2021-01-03 22:43:34 +0800

    demo

---
+++ demo01.http/demo01.client/src/main/java/demo01/client/ClientApp.java
@@ -0,0 +1,20 @@
+package demo01.client;
+
+import demo01.HelloService;
+import org.noear.nami.Nami;
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/1/3 created
+ */
+public class ClientApp {
+    public static void main(String[] args) {
+        Solon.start(ClientApp.class, args);
+
+        HelloService helloService = Nami.builder()
+                .upstream(() -> "http://localhost:8080").create(HelloService.class);
+
+        String result = helloService.hello("noear");
+        System.out.println("Rpc result: " + result);
+    }
+}
