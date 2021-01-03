66858f9d513b733f576bbce8fdb2d362322f912d
noear <noear@live.cn>
2021-01-03 23:01:48 +0800

    demo

--- demo02.socket/demo02.server/src/main/java/demo/server/HelloServiceImpl.java
+++ demo02.socket/demo02.server/src/main/java/demo/server/HelloServiceImpl.java
@@ -3,11 +3,12 @@ package demo.server;
 import demo.HelloService;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.MethodType;
 
 /**
  * @author noear 2021/1/3 created
  */
-@Mapping("/")
+@Mapping(value = "/",method = MethodType.SOCKET)
 @Component(remoting = true)
 public class HelloServiceImpl implements HelloService {
     @Override
