ca88181c75be9b89831d22f921fa0bb421d56d20
noear <noear@live.cn>
2021-01-03 23:27:59 +0800

    demo

---
+++ demo04.hybrid/demo04.client/src/main/java/demo/client/ClientApp.java
@@ -0,0 +1,67 @@
+package demo.client;
+
+import demo.HelloService;
+import org.noear.nami.Nami;
+import org.noear.nami.coder.hession.HessianDecoder;
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/1/3 created
+ */
+public class ClientApp {
+    public static void main(String[] args) {
+        Solon.start(ClientApp.class, args);
+
+        HelloService rpc = null;
+        String result = null;
+
+        //------------- socket(rsocket)
+
+        // 默认使用json解码
+        //
+        rpc = Nami.builder().upstream(() -> "tcp://localhost:28080").create(HelloService.class);
+
+        result = rpc.hello("noear");
+        System.out.println("Rpc result: " + result);
+
+        // 使用hessian解码
+        //
+        rpc = Nami.builder().upstream(() -> "tcp://localhost:28080").decoder(HessianDecoder.instance).create(HelloService.class);
+
+        result = rpc.hello("noear");
+        System.out.println("Rpc result: " + result);
+
+        //------------- websocket
+
+        // 默认使用json解码
+        //
+        rpc = Nami.builder().upstream(() -> "ws://localhost:8080").create(HelloService.class);
+
+        result = rpc.hello("noear");
+        System.out.println("Rpc result: " + result);
+
+        // 使用hessian解码
+        //
+        rpc = Nami.builder().upstream(() -> "ws://localhost:8080").decoder(HessianDecoder.instance).create(HelloService.class);
+
+        result = rpc.hello("noear");
+        System.out.println("Rpc result: " + result);
+
+
+        //------------- http
+
+        // 默认使用json解码
+        //
+        rpc = Nami.builder().upstream(() -> "http://localhost:8080").create(HelloService.class);
+
+        result = rpc.hello("noear");
+        System.out.println("Rpc result: " + result);
+
+        // 使用hessian解码
+        //
+        rpc = Nami.builder().upstream(() -> "http://localhost:8080").decoder(HessianDecoder.instance).create(HelloService.class);
+
+        result = rpc.hello("noear");
+        System.out.println("Rpc result: " + result);
+    }
+}
---
+++ demo04.hybrid/demo04.server/src/main/java/demo/server/ServerApp.java
@@ -0,0 +1,12 @@
+package demo.server;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/1/3 created
+ */
+public class ServerApp {
+    public static void main(String[] args) {
+        Solon.start(ServerApp.class, args, app->app.enableSocket(true).enableWebSocketD(true));
+    }
+}
