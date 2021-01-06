dbf2c99dc637ef564a8867e92327af4ee680149a
noear <noear@live.cn>
2021-01-06 12:19:43 +0800

    demo

--- demo10.annotation/demo10.client_springboot/src/main/java/demo/client/DemoA.java
+++ demo10.annotation/demo10.client_springboot/src/main/java/demo/client/DemoA.java
@@ -2,12 +2,12 @@ package demo.client;
 
 import demo.HelloService;
 import org.noear.nami.annotation.NamiClient;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 /**
  * @author noear 2021/1/5 created
  */
-@Component
+@Controller
 public class DemoA {
     //使用固定负载器 //适用于快速演示或调试
     @NamiClient(upstream = "http://localhost:8080")
