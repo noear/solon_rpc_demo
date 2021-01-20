8757a8f8d7f03318631cba1b3328de861760b833
noear <noear@live.cn>
2021-01-20 11:35:51 +0800

    1.2.22

---
+++ demo20.solon-cloud-nacos/demo20-client/src/main/java/Config.java
@@ -0,0 +1,17 @@
+import com.zaxxer.hikari.HikariDataSource;
+import org.noear.solon.annotation.Bean;
+import org.noear.solon.annotation.Configuration;
+import org.noear.solon.cloud.annotation.CloudConfig;
+
+import javax.sql.DataSource;
+
+/**
+ * @author noear 2021/1/20 created
+ */
+@Configuration
+public class Config {
+    @Bean
+    public DataSource ds(@CloudConfig("test_db") HikariDataSource ds){
+        return ds;
+    }
+}
