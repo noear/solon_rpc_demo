29eeffb0d98af02e76ddb4a782a45e074a466a45
noear <noear@live.cn>
2021-01-22 14:38:22 +0800

    1.2.24

---
+++ demo22.solon-cloud-water/demo22-client/src/main/java/demo/controller/TestController.java
@@ -0,0 +1,33 @@
+package demo.controller;
+
+import demo.protocol.HelloService;
+import org.noear.nami.annotation.NamiClient;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.cloud.annotation.CloudConfig;
+
+/**
+ * @author noear 2020/12/28 created
+ */
+@Controller
+public class TestController {
+    @CloudConfig("water_cache_header")
+    String water_cache_header;
+
+    //这是本地的
+    @Inject
+    HelloService helloService;
+
+    //这是远程的
+    @NamiClient
+    HelloService helloService2;
+
+    @Mapping("/test")
+    public String home(String msg) throws Exception {
+        helloService.hello();
+        String temp = helloService2.hello();
+
+        return temp;
+    }
+}
