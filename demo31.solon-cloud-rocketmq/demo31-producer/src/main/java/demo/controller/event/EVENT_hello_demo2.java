021e2d27873a7abebaf56c7f9995875f1d02ab95
noear <noear@live.cn>
2021-01-28 15:41:09 +0800

    1.3.1

--- demo31.solon-cloud-rocketmq/demo31-producer/src/main/java/demo/controller/event/EVENT_hello_demo2.java
+++ demo31.solon-cloud-rocketmq/demo31-producer/src/main/java/demo/controller/event/EVENT_hello_demo2.java
@@ -15,6 +15,7 @@ public class EVENT_hello_demo2 implements CloudEventHandler {
     @Override
     public boolean handler(Event event) throws Throwable {
         System.out.println(LocalDateTime.now() + ONode.stringify(event));
-        return event.times() > 3;
+
+        return event.times() > 2;
     }
 }
