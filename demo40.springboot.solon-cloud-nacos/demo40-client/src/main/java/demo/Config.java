2699ccb35c4d3e74996c28dba9e624e5733b9d40
noear <noear@live.cn>
2021-03-02 07:53:26 +0800

    1.3.10

---
+++ demo40.springboot.solon-cloud-nacos/demo40-client/src/main/java/demo/Config.java
@@ -0,0 +1,28 @@
+package demo;
+
+import org.noear.nami.Nami;
+import org.noear.nami.NamiConfiguration;
+import org.noear.nami.annotation.NamiClient;
+import org.noear.nami.common.Constants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author noear 2021/3/2 created
+ */
+@Configuration
+public class Config {
+    @Bean
+    public NamiConfiguration nami() {
+        return new NamiConfiguration() {
+            @Override
+            public void config(NamiClient client, Nami.Builder builder) {
+                builder.headerSet(Constants.HEADER_CONTENT_TYPE, Constants.CONTENT_TYPE_JSON);
+
+                //如果要用PROTOBUF格式
+//                builder.headerSet(Constants.HEADER_CONTENT_TYPE,Constants.CONTENT_TYPE_PROTOBUF);
+//                builder.headerSet(Constants.HEADER_ACCEPT,Constants.CONTENT_TYPE_PROTOBUF);
+            }
+        };
+    }
+}
