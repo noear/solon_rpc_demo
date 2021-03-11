abc11fc3afcd45843b6b2989fd8bd1b319306e1b
noear <noear@live.cn>
2021-03-11 13:54:41 +0800

    1.3.13

--- demo00.simple/demo00.server/src/main/java/demo/server/HelloServiceImpl.java
+++ demo00.simple/demo00.server/src/main/java/demo/server/HelloServiceImpl.java
@@ -7,8 +7,6 @@ import org.noear.solon.annotation.Mapping;
 /**
  * @author noear 2021/1/3 created
  */
-@Mapping("/")
-@Component(remoting = true)
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(String name) {
--- demo00.simple/demo00.server/src/main/java/demo/server/ServerApp.java
+++ demo00.simple/demo00.server/src/main/java/demo/server/ServerApp.java
@@ -1,12 +1,16 @@
 package demo.server;
 
 import org.noear.solon.Solon;
+import org.noear.solon.SolonApp;
 
 /**
  * @author noear 2021/1/3 created
  */
 public class ServerApp {
     public static void main(String[] args) {
-        Solon.start(ServerApp.class, args);
+        SolonApp app = Solon.start(ServerApp.class, args);
+
+        //手动添加Rpc服务（注意 remoting 属性，设为 true 即是 rpc 服务）
+        app.add("/", HelloServiceImpl.class, true);
     }
 }
