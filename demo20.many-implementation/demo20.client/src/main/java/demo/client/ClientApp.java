6723ed9ba2de18c3bd3a6a7bea8e8d74ad6df632
noear <noear@live.cn>
2021-04-07 10:08:28 +0800

    1.3.19

--- demo20.many-implementation/demo20.client/src/main/java/demo/client/ClientApp.java
+++ demo20.many-implementation/demo20.client/src/main/java/demo/client/ClientApp.java
@@ -4,6 +4,8 @@ import demo.HelloService;
 import org.noear.nami.Nami;
 import org.noear.nami.annotation.NamiClient;
 import org.noear.solon.Solon;
+import org.noear.solon.cloud.CloudClient;
+import org.noear.solon.core.LoadBalance;
 
 /**
  * @author noear 2021/1/3 created
@@ -17,7 +19,7 @@ public class ClientApp {
         //
         // 默认使用json解码
         //
-        test("demoapi1", null);  //注解可用：@NamiClient(name = "demoapi1")
+        test("demoapi1", null);   //注解可用：@NamiClient(name = "demoapi1")
         test("demoapi2", null);   //注解可用：@NamiClient(name = "demoapi2")
 
         test("demoapi1", "/v2/"); //注解可用：@NamiClient(name = "demoapi1", path = "/v2/")
@@ -25,11 +27,17 @@ public class ClientApp {
     }
 
     private static void test(String service, String path) {
+        LoadBalance lb = LoadBalance.get(service);
+
         HelloService helloService = Nami.builder()
-                .name(service)
+                .upstream(lb::getServer)
                 .path(path).create(HelloService.class);
 
+        if (path == null) {
+            path = "/";
+        }
+
         String result = helloService.hello("noear");
-        System.out.println("Rpc @" + service + " result: " + result);
+        System.out.println("Rpc @" + service + path + " result: " + result);
     }
 }
