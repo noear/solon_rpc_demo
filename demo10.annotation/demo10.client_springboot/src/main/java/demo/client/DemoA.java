5b3737e5ef8bfce7c6c4e74f084c2bfffe0ba6aa
noear <noear@live.cn>
2021-07-22 13:47:42 +0800

    1.5.15

--- demo10.annotation/demo10.client_springboot/src/main/java/demo/client/DemoA.java
+++ demo10.annotation/demo10.client_springboot/src/main/java/demo/client/DemoA.java
@@ -3,10 +3,12 @@ package demo.client;
 import demo.HelloService;
 import org.noear.nami.annotation.NamiClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author noear 2021/1/5 created
  */
+@RestController
 @Component
 public class DemoA {
     //使用固定负载器 //适用于快速演示或调试
